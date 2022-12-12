797d74df41cc1f4cd8a3c6a07a83bb541bdee2a1
ALeksandrov <a3alex89@gmail.com>
2022-12-12 19:09:24 +0200

    updated car views

--- src/app/features/cars/cars.module.ts
+++ src/app/features/cars/cars.module.ts
@@ -4,6 +4,7 @@ import { CarsComponent } from './cars/cars.component';
 import { CarDetailsComponent } from './car-details/car-details.component';
 import { carsRoutingModule } from './cars-routing.module';
 import { CarMarksComponent } from './car-marks/car-marks.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -16,6 +17,7 @@ import { CarMarksComponent } from './car-marks/car-marks.component';
   imports: [
     CommonModule,
     carsRoutingModule,
+    SharedModule
   ]
 })
 export class CarsModule { }
