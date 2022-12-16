5202bf0f895db257d6bceb97f57f6b2ac256ef9a
ALeksandrov <a3alex89@gmail.com>
2022-12-16 12:47:23 +0200

    updated theme interface

--- src/app/features/interfaces/theme.ts
+++ src/app/features/interfaces/theme.ts
@@ -2,9 +2,11 @@ export interface ITheme {
     _id: string;
     title: string;
     content: string;
+    author: string;
     posts: {
         _id: string;
         content: string;
+        author: string;
         createdAt: string;
         updatedAt: string;
     },
