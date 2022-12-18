6a122851bae6401d7bfc951401fdc015ad3c55d0
ALeksandrov <a3alex89@gmail.com>
2022-12-18 18:57:21 +0200

    updated components

--- src/app/features/forum/theme-content/theme-content.component.ts
+++ src/app/features/forum/theme-content/theme-content.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { ITheme, IUser } from '../../interfaces';
+import { ForumService } from '../forum.service';
 
 @Component({
   selector: 'app-theme-content',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemeContentComponent implements OnInit {
 
-  constructor() { }
+  theme!: ITheme;
+  hasUser!: boolean;
+
+  constructor(private activatedRoute: ActivatedRoute, private forumService: ForumService, private authService: AuthService) { }
 
   ngOnInit(): void {
-  }
+    
+    this.authService.isLoggedIn.subscribe(hasUser => this.hasUser = hasUser);
 
+    const themeId = this.activatedRoute.snapshot.params['themeId'];
+    this.forumService.getThemeById(themeId).subscribe({
+      next: (theme) => {
+        this.theme = theme;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
--- src/app/features/forum/themes/themes.component.ts
+++ src/app/features/forum/themes/themes.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { ITheme } from '../../interfaces';
+import { ForumService } from '../forum.service';
 
 @Component({
   selector: 'app-themes',
   templateUrl: './themes.component.html',
   styleUrls: ['./themes.component.scss']
 })
+
 export class ThemesComponent implements OnInit {
 
-  constructor() { }
+  themes!: ITheme[];
+  hasUser!: boolean;
+
+  constructor(private forumService: ForumService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.isLoggedIn.subscribe(hasUser => this.hasUser = hasUser);
+    this.forumService.getAllThemes().subscribe({
+      next: (themes) => {
+        this.themes = themes;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
   }
-
 }
